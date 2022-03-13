d9c886a85fefb6e549e1a5264efc7851ada52380
Alexi Chepura <a.chepura@gmail.com>
2022-03-13 12:54:19 +0300

    cones for arrows

--- src/field.rs
+++ src/field.rs
@@ -1,9 +1,11 @@
 use bevy::{
     math::Vec3,
     pbr::{PbrBundle, StandardMaterial},
-    prelude::{shape, AssetServer, Assets, Color, Commands, Mesh, Res, ResMut, Transform},
+    prelude::{
+        shape, AssetServer, Assets, BuildChildren, Color, Commands, Mesh, Res, ResMut, Transform,
+    },
 };
-use parry3d::shape::Cylinder;
+use parry3d::shape::{Cone, Cylinder};
 
 use crate::mesh::bevy_mesh;
 
@@ -19,21 +21,35 @@ pub fn field_system(
     //     ..Default::default()
     // });
 
-    for x in 0..5 {
-        for y in 0..5 {
-            for z in 0..5 {
+    for x in 0..3 {
+        for y in 0..3 {
+            for z in 0..3 {
+                let color = Color::rgb(0.1, 0.5, 0.1);
                 let cylinder = Cylinder::new(0.02, 0.002);
-                // let cylinder = Cylinder::new(1., 0.1);
                 let cylinder_mesh = bevy_mesh(cylinder.to_trimesh(10));
-                let _wheel = commands.spawn_bundle(PbrBundle {
-                    transform: Transform {
-                        translation: Vec3::new(0.1 * x as f32, 0.1 * y as f32, 0.1 * z as f32),
+                let _arrow = commands
+                    .spawn_bundle(PbrBundle {
+                        transform: Transform {
+                            translation: Vec3::new(0.1 * x as f32, 0.1 * y as f32, 0.1 * z as f32),
+                            ..Default::default()
+                        },
+                        mesh: meshes.add(cylinder_mesh),
+                        material: materials.add(color.into()),
                         ..Default::default()
-                    },
-                    mesh: meshes.add(cylinder_mesh),
-                    material: materials.add(Color::rgb(0.1, 0.5, 0.1).into()),
-                    ..Default::default()
-                });
+                    })
+                    .with_children(|parent| {
+                        let cone = Cone::new(0.01, 0.005);
+                        let cone_mesh = bevy_mesh(cone.to_trimesh(10));
+                        parent.spawn_bundle(PbrBundle {
+                            transform: Transform {
+                                translation: Vec3::new(0.0, 0.02, 0.0),
+                                ..Default::default()
+                            },
+                            mesh: meshes.add(cone_mesh),
+                            material: materials.add(color.into()),
+                            ..Default::default()
+                        });
+                    });
             }
         }
     }
